b83227e5ba80159ff99d7be7ca651a1df3c187f6
桂素伟 <axzxs2001@163.com>
2017-10-31 21:35:39 +0800

    权限管理项目。

---
+++ AspNetCore2.0Courses/PermissionDemo/Controllers/HomeController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PermissionDemo.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authentication;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication.Cookies;
+
+namespace PermissionDemo.Controllers
+{
+
+    [Authorize(Roles = "admin")]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+        [Authorize(Roles = "system,admin")]
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }   
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        [AllowAnonymous]
+        [HttpGet("login")]
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [AllowAnonymous]
+        [HttpPost("login")]
+        public IActionResult Login(string userName, string password)
+        {
+            if (userName == "gsw" && password == "123")
+            {
+                var claims = new Claim[] {
+                    new Claim(ClaimTypes.Role,"admin"),
+                    new Claim(ClaimTypes.Name,"桂素伟")
+                };
+                HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(new ClaimsIdentity(claims)));
+
+
+                return new RedirectResult("/home/index");
+            }
+            else
+            {
+                ViewBag.error = "用户名或密码错误";
+                return View();
+            }
+        }
+    }
+}
---
+++ AspNetCore2.0Courses/PermissionMiddlewareDemo/Models/PermissionMiddleware.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PermissionMiddlewareDemo.Models
+{
+    public class PermissionMiddleware
+    {
+
+        readonly RequestDelegate _next;
+        public PermissionMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+        public Task Invoke(HttpContext context)
+        {
+            //context.Request.Cookies
+            var path = context.Request.Path;
+            //处理权限问题
+            //context.Response.Redirect("/denied");
+
+            return _next(context);
+        }
+    }
+}

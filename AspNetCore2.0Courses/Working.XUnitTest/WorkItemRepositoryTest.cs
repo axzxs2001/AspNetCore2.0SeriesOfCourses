e2600c27791cfb53f28ed98cf948e97d04a1a34d
桂素伟 <axzxs2001@163.com>
2017-11-27 21:11:57 +0800

    添加工作项测试。

--- AspNetCore2.0Courses/Working.XUnitTest/WorkItemRepositoryTest.cs
+++ AspNetCore2.0Courses/Working.XUnitTest/WorkItemRepositoryTest.cs
@@ -34,11 +34,41 @@ namespace Working.XUnitTest
         [Fact]
         public void GetWorkItemByYearMonth_Default_Return()
         {
-            var list = new List<WorkItem>() { new WorkItem { CreateUserID=1 } };
-            _dbMock.Setup(db => db.Query<WorkItem>(It.IsAny<string>(),It.IsAny<object>(),null,true, null, null)).Returns(list);
-            var workitems = _workItemRepository.GetWorkItemByYearMonth(2017,10,1);
-            Assert.Equal(31,workitems.Count);
+            var list = new List<WorkItem>() { new WorkItem { CreateUserID = 1 } };
+            _dbMock.Setup(db => db.Query<WorkItem>(It.IsAny<string>(), It.IsAny<object>(), null, true, null, null)).Returns(list);
+            var workitems = _workItemRepository.GetWorkItemByYearMonth(2017, 10, 1);
+            Assert.Equal(31, workitems.Count);
         }
+        /// <summary>
+        /// 抛出未知异常测试
+        /// </summary>
+        [Fact]
+        public void GetWorkItemByYearMonth_ThrowException_ReturnException()
+        {
+            var list = new List<WorkItem>() { new WorkItem { CreateUserID = 1 } };
+            _dbMock.Setup(db => db.Query<WorkItem>(It.IsAny<string>(), It.IsAny<object>(), null, true, null, null)).Throws(new Exception("未知异常"));
+            var exce = Assert.Throws<Exception>(() => _workItemRepository.GetWorkItemByYearMonth(2017, 10, 1));
+            Assert.Equal("未知异常", exce.Message);
+        }
+        /// <summary>
+        /// 添加工作项测试
+        /// </summary>
+        /// <param name="workItemID">工作项ID</param>
+        /// <param name="returnResult">返回值</param>
+        [Theory]
+        [InlineData(1,1)]
+        [InlineData(2,0)]
+        public void AddWorkItem_Default_ReturnTrue(int workItemID ,int returnResult)
+        {
+            var list = new List<WorkItem>() { new WorkItem { CreateUserID = 1 } };
+            _dbMock.Setup(db => db.Query<WorkItem>(It.IsAny<string>(), It.IsAny<object>(), null, true, null, null)).Returns(list);
 
+
+
+            _dbMock.Setup(db => db.Execute(It.IsAny<string>(), It.IsAny<object>(),null,null,null)).Returns(value:returnResult);
+
+            var result = _workItemRepository.AddWorkItem(new WorkItem { ID = workItemID }, 1);
+            Assert.True(result==(workItemID==1));
+        }
     }
 }

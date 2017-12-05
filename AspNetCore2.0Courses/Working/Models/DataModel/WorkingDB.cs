e4491f60aa276ebdb8a27faaa9eed41f35c4cb89
桂素伟 <axzxs2001@163.com>
2017-12-05 08:42:20 +0800

    workingdb添加注释。

--- AspNetCore2.0Courses/Working/Models/DataModel/WorkingDB.cs
+++ AspNetCore2.0Courses/Working/Models/DataModel/WorkingDB.cs
@@ -29,13 +29,13 @@ namespace Working.Models.DataModel
         /// <summary>
         /// 查询方法
         /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="sql"></param>
-        /// <param name="param"></param>
-        /// <param name="transaction"></param>
-        /// <param name="buffered"></param>
-        /// <param name="commandTimeout"></param>
-        /// <param name="commandType"></param>
+        /// <typeparam name="T">映射实体类</typeparam>
+        /// <param name="sql">sql语句</param>
+        /// <param name="param">参数对象</param>
+        /// <param name="transaction">事务</param>
+        /// <param name="buffered">是否缓存结果</param>
+        /// <param name="commandTimeout">command超时时间(秒)</param>
+        /// <param name="commandType">command类型</param>
         /// <returns></returns>
         public IEnumerable<T> Query<T>(string sql, object param = null, IDbTransaction transaction = null, bool buffered = true, int? commandTimeout = null, CommandType? commandType = null)
         {
@@ -44,11 +44,11 @@ namespace Working.Models.DataModel
         /// <summary>
         /// 执行方法
         /// </summary>
-        /// <param name="sql"></param>
-        /// <param name="param"></param>
-        /// <param name="transaction"></param>
-        /// <param name="commandTimeout"></param>
-        /// <param name="commandType"></param>
+        /// <param name="sql">映射实体类</param>
+        /// <param name="param">参数对象</param>
+        /// <param name="transaction">事务</param>
+        /// <param name="commandTimeout">command超时时间(秒)</param>
+        /// <param name="commandType">command类型</param>
         /// <returns></returns>
         public int Execute(string sql, object param = null, IDbTransaction transaction = null, int? commandTimeout = null, CommandType? commandType = null)
         {
